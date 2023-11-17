091a784b9f4b11b59f553f3fe0749b52d2f571f2
Sinsamut Khunphimool <deathbullet47@gmail.com>
2023-11-17 15:15:49 +0700

    Lab0

---
+++ Lab0/Lab0/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Lab0
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(343, 80);
+            label1.Name = "label1";
+            label1.Size = new Size(162, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Sinsamut Khunphimool";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(343, 185);
+            label2.Name = "label2";
+            label2.Size = new Size(169, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Student ID 663450310-9";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(343, 274);
+            label3.Name = "label3";
+            label3.Size = new Size(240, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Computer and Information Science";
+            label3.Click += label3_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources._400302919_1599925687413344_787882702878873387_n;
+            pictureBox1.Location = new Point(26, 14);
+            pictureBox1.Margin = new Padding(5);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(298, 405);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private PictureBox pictureBox1;
+    }
+}
